a266d7ba2bbbb680792e89516399840728b6ae87
ingvillajimenez <ing.villa.jimenez@gmail.com>
2019-01-29 12:56:57 -0600

    spring 3 routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,31 @@
 const { Router } = require('express');
 const app = Router();
 
-// const Companies = require('../controllers/companies/companies');
+const Users = require('../controllers/users/users');
+const Treatments = require('../controllers/treatments/treatments');
+const Appointments = require('../controllers/appointments/appointments');
 
-//company routes
-//app.get('/companies', Companies.index);
-//app.get('/companies/:companyId', Companies.findBy);
+// user routes
+app.get('/users', Users.index);
+app.get('/users/:userId', Users.findBy);
+app.get('/users/:userId/treatments', Users.findTreatmentsBy)
+app.post('/users', Users.create);
+app.put('/users/:userId', Users.updateBy);
+// app.delete('/users/:userId', Users.removeBy);
 
+// treatment routes
+app.get('/treatments', Treatments.index);
+app.get('/treatments/:treatmentId', Treatments.findBy);
+app.get('/treatments/:treatmentId/appointments', Treatments.findAppointmentsBy);
+app.post('/treatments', Treatments.create);
+app.put('/treatments/:treatmentId', Treatments.updateBy);
+// app.delete('/treatment/:userId', Treatment.removeBy);
 
+// appoinment routes
+app.get('/appointments', Appointments.index);
+app.get('/appointments/:appointmentsId', Appointments.findBy);
+// app.post('/appointments', Appointments.create);
+// app.put('/appointments/:appointmentsId', Appointments.updateBy);
+// app.delete('/treatment/:userId', Treatment.removeBy);
 
 module.exports = app;
